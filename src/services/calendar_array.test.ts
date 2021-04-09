15d35c3d1659ebbb6b05d7db1ebce46537bcbcdd
sakanobu <grapejuice100percent@gmail.com>
2021-04-09 21:24:32 +0900

    Fixed about preparing expected values in test

--- src/services/calendar_array.test.ts
+++ src/services/calendar_array.test.ts
@@ -1,52 +1,53 @@
 import { createCalendarArray } from './calendar_array';
 
-const expected = [
-  '28',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  '11',
-  '12',
-  '13',
-  '14',
-  '15',
-  '16',
-  '17',
-  '18',
-  '19',
-  '20',
-  '21',
-  '22',
-  '23',
-  '24',
-  '25',
-  '26',
-  '27',
-  '28',
-  '29',
-  '30',
-  '31',
-  '1',
-  '2',
-  '3',
-].map((day: string, i: number) => {
-  return {
-    day,
-    indexForKey: i,
-    selectedMonth: i >= 1 && i <= 31 ? true : false,
-  };
-});
-
 describe('createCalendarArray', () => {
   it('2021/03 calendar', () => {
+    const expected = [
+      '28',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      '11',
+      '12',
+      '13',
+      '14',
+      '15',
+      '16',
+      '17',
+      '18',
+      '19',
+      '20',
+      '21',
+      '22',
+      '23',
+      '24',
+      '25',
+      '26',
+      '27',
+      '28',
+      '29',
+      '30',
+      '31',
+      '1',
+      '2',
+      '3',
+    ].map((day: string, i: number) => {
+      return {
+        day,
+        indexForKey: i,
+        selectedMonth: i >= 1 && i <= 31 ? true : false,
+      };
+    });
+
     const result = createCalendarArray(new Date(2021, 2));
+
     expect(result).toStrictEqual(expected);
   });
 });
