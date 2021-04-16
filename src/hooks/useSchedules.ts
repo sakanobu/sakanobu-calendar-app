dc17d7d0f6bc49458bada0aba1ade0b620452be8
sakanobu <grapejuice100percent@gmail.com>
2021-04-16 18:43:35 +0900

    Add custom hooks to fetch data from firebase every time the calendar month changes

--- src/hooks/useSchedules.ts
+++ src/hooks/useSchedules.ts
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import React from 'react';
+import firebase from 'firebase/app';
+import { getSchedules } from 'services/request_firebase';
 
 export type ScheduleWithUserTagColor = {
   title: string;
@@ -15,4 +17,25 @@ export type ScheduleWithUserTagColor = {
       theme: string;
     };
   };
+};
+
+type UseTodo = {
+  schedules: ScheduleWithUserTagColor[];
+};
+
+// useScheduleの引数として指定した日付を渡す必要あるっぽい(コレクション内の全ドキュメントは不要なので)
+export const useSchedule = (selectedDate: Date): UseTodo => {
+  const [schedules, setSchedules] = React.useState<ScheduleWithUserTagColor[]>(
+    []
+  );
+
+  React.useEffect(() => {
+    (async () => {
+      const newSchedules = await getSchedules(selectedDate);
+
+      setSchedules(newSchedules);
+    })();
+  }, [selectedDate]);
+
+  return { schedules };
 };
