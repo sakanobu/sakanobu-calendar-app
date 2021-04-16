be8c2e4d7be697fe823220cb55f856e95f216ada
sakanobu <grapejuice100percent@gmail.com>
2021-04-17 05:26:45 +0900

    Add feature of fetching tags with color data from Firebase

--- src/services/request_firebase.ts
+++ src/services/request_firebase.ts
@@ -95,3 +95,24 @@ export const getSchedules = async (
     })
   );
 };
+
+export const getTagsWithColor = async (): Promise<TagWithColor[]> => {
+  const tagsQuerySnapshot = await db.collection('tags').get();
+
+  return await Promise.all(
+    tagsQuerySnapshot.docs.map(async (doc) => {
+      const tag = doc.data() as Tag;
+
+      const color = (await tag.colorRef.get()).data() as Color;
+
+      return {
+        name: tag.name,
+        checked: tag.checked,
+        selectedColor: {
+          name: color.name,
+          theme: color.theme,
+        },
+      };
+    })
+  );
+};
